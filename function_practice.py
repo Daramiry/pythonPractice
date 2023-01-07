e4e0e4037e6ea23db997ef0dcc38e0ec0a290860
dany ramirez <drz0224@gmail.com>
2023-01-07 10:48:35 -0800

    finished

---
+++ function_practice.py
@@ -0,0 +1,23 @@
+def hello():
+    print("greetings")
+
+def pack (box, tape, item):
+        return [box, tape, item]
+
+
+def eat_lunch(my_lst):
+  if len(my_lst) == 0:
+    print("My lunchbox is empty!")
+  else:
+    for i in range(len(my_lst)):
+      if i == 0:
+        print(f"First I eat {my_lst[0]}")
+      else:
+        print(f"Next I eat {my_lst[i]}")
+
+        
+print(pack("box","tape", "item"))
+hello()
+eat_lunch([])
+eat_lunch(["sandwich"])
+eat_lunch(["apple","banana","sandwich","cookies"])
